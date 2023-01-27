d28e36bb6f8ddfd0afd98027ec9605296c71fe10
devforfu <developer.z@outlook.com>
2023-01-27 23:51:40 +0100

    Deref example

---
+++ bin/book.rs
@@ -0,0 +1,35 @@
+use std::io::{stdin, BufRead};
+use interfaces::refs::{Name, PhoneNumber, PhoneBook};
+
+fn main() -> std::io::Result<()> {
+    let mut book = PhoneBook::default();
+    for (name, phone) in read_names()?.into_iter().flat_map(parse) {
+        book.add(name, phone);
+    }
+    println!("Book size: {}", book.len());
+    Ok(())
+}
+
+fn read_names() -> std::io::Result<Vec<String>> {
+    let mut names = Vec::new();
+    let mut buf = String::new();
+    while let Ok(n) = stdin().lock().read_line(&mut buf) {
+        buf = buf.trim().to_string();
+        if n == 0 || buf == "EOF" { break; }
+        names.push(buf);
+        buf = String::new();
+    }
+    Ok(names)
+}
+
+fn parse(entry: String) -> Option<(Name, PhoneNumber)> {
+    let mut split = entry.split(':');
+    if let Some(name) = split.next() {
+        if let Some(phone) = split.next() {
+            if let Ok(phone) = phone.parse::<usize>() {
+                return Some((name.to_string(), phone))
+            }
+        }
+    }
+    None
+}
---
+++ src/refs.rs
@@ -0,0 +1,41 @@
+use std::collections::hash_map::HashMap;
+use std::ops::Deref;
+
+pub type Name = String;
+pub type PhoneNumber = usize;
+
+pub struct PhoneBook(HashMap<Name, PhoneNumber>);
+
+impl PhoneBook {
+    pub fn add(&mut self, name: Name, number: PhoneNumber) {
+        self.0.insert(name, number);
+    }
+}
+
+impl Default for PhoneBook {
+    fn default() -> Self {
+        Self { 0: HashMap::new() }
+    }
+}
+
+impl Deref for PhoneBook {
+    type Target = HashMap<Name, PhoneNumber>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_phone_book() {
+        let mut book = PhoneBook::default();
+        book.add(String::from("Alice"), 123456789);
+        assert_eq!(book.len(), 1);
+    }
+}
+
