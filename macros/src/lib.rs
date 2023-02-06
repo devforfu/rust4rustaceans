701c8d5c654aa5fc9932d0a84bf18f0424329c06
devforfu <developer.z@outlook.com>
2023-02-06 23:56:13 +0100

    [WIP]

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -5,6 +5,16 @@ fn test_inner<T: Debug>(init: T, flag: bool) {
     println!("type={:?}, init={:?}, flag={:?}", type_name::<T>(), init, flag);
 }
 
+fn test_clone<T: Clone>() {}
+
+macro_rules! clone_from_copy {
+    ($($t:ty),*) => {
+        $(impl Clone for $t {
+            fn clone(&self) -> Self { *self }
+        })*
+    }
+}
+
 macro_rules! test_battery {
   ($($t:ty as $name:ident),*) => {
 $(
@@ -18,10 +28,34 @@ mod $name {
 } )*
 } }
 
+macro_rules! test_trait {
+  ($($t:ty as $name:ident),*) => {
+$(
+#[cfg(test)]
+mod $name {
+    use super::test_clone;
+    #[test]
+    fn implements_clone() { test_clone::<$t>() }
+} )*
+} }
+
 test_battery! (
     u8 as u8_test,
     u16 as u16_test,
     u32 as u32_test,
     u64 as u64_test,
     u128 as u128_test
+);
+
+#[derive(Copy)]
+struct Integer(i32);
+
+#[derive(Copy)]
+struct Float(f32);
+
+clone_from_copy![Integer, Float];
+
+test_trait! (
+    Integer as integer_clone,
+    Float as float_clone
 );
